3ddf7fe7d22eeb4f198e7831ba5f1b40fe395ea0
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-09 14:53:21 +0300

    поиск индекса

--- example_013_поиск индекса/Program.cs
+++ example_013_поиск индекса/Program.cs
@@ -1,12 +1,13 @@
 ﻿int[] array = {1,3,4,67,4147,4,7,521};
 int n = array.Length;
 int index = 0;
-int find = 4147;
+int find = 4;
 while (index<n)
 {
     if (array [index] == find)
     {
         Console.WriteLine(index);
+        break;
     }
     index++;
 
