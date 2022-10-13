0904cb6c5c499e9e93f156462e3503894ba7814e
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-13 22:31:13 +0300

    метод

---
+++ example_S_4_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+Console.Clear();
+int A = new Random().Next();
+int CountNumbers = GetCountNumbers(A);
+// Console.WriteLine (A);
+// while (A > 0)
+// {A = A/10;
+// a ++;}
+Console.WriteLine($"{A} ->{CountNumbers}");
+int GetCountNumbers (int A)
+{
+        int count = 0;
+        while (A > 0)
+        {
+            A = A/10;
+            count ++;
+        }
+        return count;
+}
+    
