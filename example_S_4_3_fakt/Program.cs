bfbbc05184ebe97c40307126729c15431906b1c6
Aigul <gayazovaaigulgazimovna@gmail.com>
2022-10-13 22:09:26 +0300

    factorial

---
+++ example_S_4_3_fakt/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+Console.Clear();
+
+Console.WriteLine ("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int fac = GetFactirial(N);
+Console.Write(fac);
+
+int GetFactirial(int N)
+{
+
+    int a = 1;
+    int fac = 1;
+    {
+        while (a<=N)
+        {
+            fac = a * fac;
+             a++;
+        }
+        return fac;
+    }
+} 
+
+
+
